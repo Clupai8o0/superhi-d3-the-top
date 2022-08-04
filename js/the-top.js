956a5427fccd0e75af85fc7ce1ab4c867812db7b
Clupai8o0 <clupaio4@gmail.com>
2022-08-04 12:05:25 +0300

    list layout styling

--- js/the-top.js
+++ js/the-top.js
@@ -1,5 +1,6 @@
 const svg = d3.select("svg");
-svg.attr("height", 40 * data.length);
+
+svg.attr("height", 40 * data.length).attr("width", 960);
 
 const groups = svg
 	.selectAll("g.movie")
@@ -9,4 +10,16 @@ const groups = svg
 	.attr("class", "movie")
 	.attr("transform", (_, i) => `translate(0, ${i * 40})`);
 
-groups.append("text").text((d) => d.title);
+groups
+	.append("text")
+	.attr("x", 90)
+	.attr("y", 20)
+	.attr("class", "title")
+	.text((d) => d.title);
+
+groups
+	.append("text")
+	.attr("x", 24)
+	.attr("y", 20)
+  .attr("class", "year")
+	.text((d) => d.year);
