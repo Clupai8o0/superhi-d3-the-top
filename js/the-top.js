c8bc6b6b0fb685a7004e2e6b7717f568363c6d3f
Clupai8o0 <clupaio4@gmail.com>
2022-08-04 12:39:26 +0300

    plotting the data

--- js/the-top.js
+++ js/the-top.js
@@ -2,6 +2,8 @@ const svg = d3.select("svg");
 
 svg.attr("height", 40 * data.length).attr("width", 960);
 
+const scoreScale = d3.scaleLinear().domain([0, 100]).range([420, 900]);
+
 const groups = svg
 	.selectAll("g.movie")
 	.data(data)
@@ -21,5 +23,19 @@ groups
 	.append("text")
 	.attr("x", 24)
 	.attr("y", 20)
-  .attr("class", "year")
+	.attr("class", "year")
 	.text((d) => d.year);
+
+groups
+	.append("circle")
+	.attr("cx", (d) => scoreScale(d.metascore))
+	.attr("cy", 20)
+	.attr("r", 8)
+	.attr("class", "metascore");
+
+groups
+	.append("circle")
+	.attr("cx", (d) => scoreScale(d.imdb))
+	.attr("cy", 20)
+	.attr("r", 8)
+	.attr("class", "imdb");
